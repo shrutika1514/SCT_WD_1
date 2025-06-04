cefb7903d554d669ce9e75ba385de7a58d2e9880
shrutika1514 <shrutikaadisare3725@gmail.com>
2025-06-04 20:10:58 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,8 @@
+// Cookie accept/decline buttons
+document.querySelector(".accept").addEventListener("click", () => {
+    document.querySelector(".cookie-banner").style.display = "none";
+  });
+  
+  document.querySelector(".decline").addEventListener("click", () => {
+    document.querySelector(".cookie-banner").style.display = "none";
+  });
